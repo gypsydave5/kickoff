87ea03f6c09749ea1ead1093f6ef9058719c8c13
David Wickes <dave.wickes@gmail.com>
2022-06-14 22:26:40 +0100

    improvement to text questioner test

---
+++ test/text_questioner_test.go
@@ -0,0 +1,34 @@
+package kickoff
+
+import (
+	"fmt"
+	"github.com/gypsydave5/kickoff"
+	"github.com/gypsydave5/kickoff/test/random"
+	"strings"
+	"testing"
+)
+
+func TestTextQuestioner_ReadingAndWriting(t *testing.T) {
+	answer := random.RandomString()
+	question := random.RandomString()
+
+	out := new(strings.Builder)
+	in := strings.NewReader(fmt.Sprintf("%s\n", answer))
+
+	q := kickoff.NewTextQuestioner(in, out)
+
+	gotAnswer, err := q.AskQuestion(kickoff.NewTextQuestion(question))
+	if err != nil {
+		t.Errorf("Unexpected error: %s", err)
+	}
+
+	if gotAnswer.String() != answer {
+		t.Errorf("Wanted %q got %q", answer, gotAnswer)
+	}
+
+	gotQuestion := out.String()
+
+	if gotQuestion != question {
+		t.Errorf("Wanted %q got %q", question, gotQuestion)
+	}
+}
