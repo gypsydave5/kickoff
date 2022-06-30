98af5b48034687153249f1a4e07cb77365a50ade
David Wickes <dave.wickes@gmail.com>
2022-06-30 09:13:32 +0100

    docs

--- handler/jira/httpclient.go
+++ handler/jira/httpclient.go
@@ -5,18 +5,21 @@ import (
 	"net/http"
 )
 
+// BasicAuthRoundTripper adds basic authentication to an http.Request and delegates to the provided Next http.RoundTripper
 type BasicAuthRoundTripper struct {
-	next         http.RoundTripper
-	encodedCreds string
+	Next         http.RoundTripper
+	EncodedCreds string
 }
 
+// NewBasicAuthRoundTripper creates a BasicAuthRoundTripper, base64 encoding the provided username and password. Requests
+// made to the Next http.RoundTripper will have a basic authentication header added to them.
 func NewBasicAuthRoundTripper(next http.RoundTripper, username string, password string) *BasicAuthRoundTripper {
-	return &BasicAuthRoundTripper{next: next, encodedCreds: basicAuth(username, password)}
+	return &BasicAuthRoundTripper{Next: next, EncodedCreds: basicAuth(username, password)}
 }
 
 func (b BasicAuthRoundTripper) RoundTrip(request *http.Request) (*http.Response, error) {
-	request.Header.Add("Authorization", "Basic "+b.encodedCreds)
-	return b.next.RoundTrip(request)
+	request.Header.Add("Authorization", "Basic "+b.EncodedCreds)
+	return b.Next.RoundTrip(request)
 }
 
 // NewBasicAuthHTTP is a convenience function that creates a default *http.Client with the BasicAuthRoundTripper applied
